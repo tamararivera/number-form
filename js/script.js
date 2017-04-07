2f27e5d4b0eeaeb852fe53f4f0390d49c815fe47
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-04-07 00:27:28 -0700

    Implemented the functionality of the submit button.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+var numbers = [];
+var sum = 0;
 function addNumber(event) {
-  console.log("done!");
+  var formNumber = document.addForm.number.value;
+  if(isNumeric(formNumber)) {
+    var number = parseFloat(formNumber);
+    numbers.push(number);
+    sum += number;
+  }
+  updateResults();
+  document.addForm.number.value = "";
   event.preventDefault();
+}
+
+function updateResults() {
+  document.getElementById("count").innerHTML = numbers.length;
+  document.getElementById("sum").innerHTML = sum;
+  document.getElementById("average").innerHTML = sum/numbers.length;
+  
+}
+
+/*Taked from stackoverflow http://stackoverflow.com/questions/18082/validate-decimal-numbers-in-javascript-isnumeric*/
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
