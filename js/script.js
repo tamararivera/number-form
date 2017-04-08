6b138dfeeb987b3e846a902baf9ff874a99938f0
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-04-07 23:42:01 -0700

    Added a graph from Google Charts.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+/* globals google */
 var numbers = [];
 var sum = 0;
 function addNumber(event) {
   var formNumber = document.addForm.number.value;
   if(isNumeric(formNumber)) {
     var number = parseFloat(formNumber);
-    numbers.push(number);
+    numbers.push([numbers.length + "", number]);
     sum += number;
   }
   updateResults();
@@ -28,10 +29,34 @@ function updateResults() {
     average = 0;
   }
   document.getElementById("average").innerHTML = average;
-  
+  if(average !== 0) {
+    drawVisualization(); 
+  } else {
+    document.getElementById('chart').innerHTML = "";
+  }
 }
 
 /*Taked from stackoverflow http://stackoverflow.com/questions/18082/validate-decimal-numbers-in-javascript-isnumeric*/
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+google.charts.load('current', {'packages':['corechart']});
+//google.charts.setOnLoadCallback(drawVisualization);
+
+function drawVisualization() {
+  var matrix = numbers.slice(0);
+  matrix.unshift(['Order', 'Number']);
+  var data = google.visualization.arrayToDataTable(matrix);
+
+  var options = {
+    title : 'Numbers',
+    vAxis: {title: 'Number'},
+    hAxis: {title: 'Order'},
+    seriesType: 'area',
+    legend: {position: 'none'}
+  };
+
+  var chart = new google.visualization.ComboChart(document.getElementById('chart'));
+  chart.draw(data, options);
 }
